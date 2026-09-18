e952a7169aa520dbf90fab9d2ca362cb5ca25a51
ricardcferreira <ricardcferreira@gmail.com>
2026-09-18 12:22:31 +0100

    fix: remove remaining app logic and fix button import

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/body_composition/05_evaluation/utils"
+import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/lib/body_composition/05_evaluation/utils"
+import { cn } from "@/lib/utils"
 
 function TooltipProvider({
   delayDuration = 0,
